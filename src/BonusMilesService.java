d23155fbbdb902e6f476d631d69ccc48ed918848
Nikita Novoselov <sarkomath@outlook.com>
2020-11-24 22:21:08 +1000

    second fix

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,9 +2,9 @@ public class BonusMilesService {
     // создан класс BonusMilesService
     public int calculate(int cost) {
         // создан класс calculate, с параметром cost типа int
-        int ticket_miles = 20;
+        int TicketMiles = 20;
         int distance = 1;
-        int miles = cost / ticket_miles * distance;
+        int miles = cost / TicketMiles * distance;
         return miles;
         // возвращает miles типа int
     }
